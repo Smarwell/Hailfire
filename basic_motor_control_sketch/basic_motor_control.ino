d061cbd9af1c76b52e2af870533c65fa201f3c6c
Smarwell <smarewell@gmail.com>
2016-11-07 10:43:59 -0600

    Added the basic control sketch

---
+++ basic_motor_control_sketch/basic_motor_control.ino
@@ -0,0 +1,24 @@
+int width;
+bool led;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2,OUTPUT);
+pinMode(13,OUTPUT);
+width=2000;
+Serial.begin(115200);
+led=false;
+}
+
+void loop() {
+if(Serial.available()){
+  width=Serial.parseInt();
+  led=!led;
+  digitalWrite(13,led);
+}
+digitalWrite(2,HIGH);
+delayMicroseconds(width);
+digitalWrite(2,LOW);
+delay(10);
+
+}
